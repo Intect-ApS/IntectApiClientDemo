cf28d992de6fc052c749300e26def81edaa78dc0
Rune Jantzen <rj@capworks.eu>
2019-08-07 14:45:47 +0200

    More usecases added

---
+++ GratisalApiClientDemo/Helpers.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace GratisalApiClientDemo
+{
+    static class Helpers
+    {
+        static Stopwatch sw = new Stopwatch();
+
+        public static void Log(this string _string)
+        {
+            if (!sw.IsRunning) sw.Start();
+            Console.WriteLine($"{sw.Elapsed.ToString(@"ss\.ff")} // {_string}");
+        }
+
+        public enum Gender
+        {
+            Male,
+            Female
+        }
+
+        public static string GenerateRandomIdentitynumber(Gender gender)
+        {
+            var weights = new[] { 4, 3, 2, 7, 6, 5, 4, 3, 2, 1 };
+            var fromdaterange = new DateTime(1920, 01, 01);
+            var todaterange = new DateTime(1999, 12, 31);
+
+            var result = string.Empty;
+
+            var randomDays = new Random().Next(0, (todaterange - fromdaterange).Days);
+
+            var randomDate = todaterange.AddDays(-randomDays).Date;
+
+            while (true)
+            {
+                var serialNumber = new Random().Next(100, 400);
+
+                result = randomDate.ToString("ddMMyy") + serialNumber;
+
+                var weightsum = 0;
+                for (var i = 0; i < weights.Length - 1; i++)
+                {
+                    weightsum += weights[i] * Convert.ToInt32(result.Substring(i, 1));
+                }
+
+                var modulus = weightsum % 11;
+
+                switch (gender)
+                {
+                    default:
+                    case Gender.Male:
+                        if (modulus % 2 == 0 && modulus != 0)
+                            return result + (11 - modulus).ToString();
+                        break;
+                    case Gender.Female:
+                        if (modulus % 2 == 1 && modulus != 1)
+                            return result + (11 - modulus).ToString();
+                        break;
+                }
+            }
+        }
+    }
+}
--- GratisalApiClientDemo/Program.cs
+++ GratisalApiClientDemo/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
+using static GratisalApiClientDemo.Helpers;
 
 namespace GratisalApiClientDemo
 {
@@ -14,7 +15,7 @@ namespace GratisalApiClientDemo
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                $"{ex.Message}".Log();
             }
         }
 
@@ -22,7 +23,7 @@ namespace GratisalApiClientDemo
         {
             gratisalapiclientlib.GratisalClient gratisalClient;
 
-            // Authorizing credentials
+            "* Authorizing credentials *".Log();
             try
             {
                 // Credentials. You can create a user to optain credentials at TEST: https://gratisaltest.dk/signup/ or LIVE: https://app.gratisal.dk/signup/
@@ -41,29 +42,158 @@ namespace GratisalApiClientDemo
                 throw new Exception("Error authorizing credentials", ex);
             }
 
+            "Done authorizing credentials\n".Log();
+
             // You can get a full list of Gratisal methods at TEST: https://api.gratisaltest.dk/swagger/ui/index or LIVE: https://api.gratisal.dk/swagger/ui/index
 
             // ***********************
             // *** EXAMPLE METHODS ***
             // ***********************
 
-            #region * Change companyuser first name *
+            #region * Initialize data *
+            "* Initialize data *".Log();
 
-            // Get list of Company users
-            var companyUsers = await gratisalClient.CompanyUsers_GetAllCompanyUsersAsync();
+            "Get available companies".Log();
+            var companyInfo = await gratisalClient.Users_GetCompaniesAsync();
 
-            // Find user with firstname 'Bob'
-            var companyUser = companyUsers.FirstOrDefault(x => x.FirstName == "Bob");
+            "Get available countries".Log();
+            var countries = await gratisalClient.StaticData_GetCountriesAsync();
 
-            // Change firstname
-            companyUser.FirstName = "James";
+            "Get available roles".Log();
+            var roles = await gratisalClient.StaticData_GetRolesAsync();
 
-            // Update firstname i gratisal
-            var companyUserUpdateResult = await gratisalClient.CompanyUsers_UpdateCompanyUserAsync(companyUser);
+            "Get available employment templates".Log();
+            var employmentTemplates = await gratisalClient.EmploymentTemplates_GetEmploymentTemplatesAsync();
+
+            "Get available languages".Log();
+            var languages = await gratisalClient.Miscellaneous_GetLanguagesAsync();
+
+            "Get available taxcard types".Log();
+            var taxcardTypes = await gratisalClient.StaticData_GetTaxCardTypesAsync();
+
+            "Get available timeentrytypes".Log();
+            var timeEntryTypes = await gratisalClient.TimeEntryTypes_GetTimeEntryTypeViewsAsync();
+
+            "Get available unittypes".Log();
+            var unitTypes = await gratisalClient.StaticData_GetUnitTypesAsync();
+
+            "Get available statuses".Log();
+            var statuses = await gratisalClient.StaticData_GetTimeEntryStatusesAsync();
+
+            "Done initializing data\n".Log();
 
             #endregion
 
-            // Terminate a session
+            #region * Create companyuser *
+            "* Create companyuser *".Log();
+
+            "Create AddUserToCompanyRequest object".Log();
+            var addUserToCompanyRequest = new Gratisal.WebAPI.Client.AddUserToCompanyRequest()
+            {
+                Details = new Gratisal.WebAPI.Client.CompanyUser()
+                {
+                    Address = new Gratisal.WebAPI.Client.Address()
+                    {
+                        CountryId = countries.First(x => x.Key == "DK").Id,
+                        Line1 = "MyStreet 2",
+                        Line2 = "1. floor to the right",
+                        PostalCode = "3344",
+                    },
+                    PersonalEmail = "js@caribbean.ky",
+                    CompanyId = companyInfo.First().CompanyId,
+                    FirstName = "Jack",
+                    MiddleName = "Teague",
+                    LastName = "Sparrow",
+                    IsActive = true,
+                    RoleId = roles.First(x => x.Key == "Employee").Id,
+                    TelFixed = "+345 1234 5678",
+                    TelMobile = "+345 8765 4321",
+                },
+                EmploymentTemplateId = employmentTemplates.First(x => x.Name == "HourlyPaid").Id,
+                HireDate = DateTime.Now,
+                IdentityNumber = GenerateRandomIdentitynumber(Gender.Male), // CPR number
+                LanguageId = languages.First(x => x.Code == "DK").Id,
+                TaxCardTypeId = taxcardTypes.First(x => x.Key == "Primary").Id,
+                Title = "Employee",
+            };
+
+            "Create Company User".Log();
+            var addUserToCompanyResponse = await gratisalClient.CompanyUsers_AddUserToCompanyAsync(addUserToCompanyRequest);
+
+            "Done creating CompanyUser\n".Log();
+
+            #endregion
+
+            #region * Change companyuser middle name *
+            "* Change companyuser middle name *".Log();
+
+            "Use created user from 'Create companyuser'".Log();
+            var companyUser = addUserToCompanyResponse.CompanyUser;
+
+            if (companyUser != null)
+            {
+                "Change middelname".Log();
+                companyUser.MiddleName = "Kirby";
+
+                "Update firstname i gratisal".Log();
+                var companyUserUpdateResult = await gratisalClient.CompanyUsers_UpdateCompanyUserAsync(companyUser);
+            }
+            else
+            {
+                "Not found".Log();
+            }
+
+            "Done changing companyuser middel name\n".Log();
+
+            #endregion
+
+            #region * Add timeregistration to companyuser *
+            "* Add timeregistration to companyuser *".Log();
+
+            "Reusing companyUsers from 'Create companyuser' example".Log();
+            companyUser = addUserToCompanyResponse.CompanyUser;
+
+            "Create TimeEntryRecord object".Log();
+            var timeEntryRecordRequest = new Gratisal.WebAPI.Client.TimeEntryRecord()
+            {
+                Description = "\"Business\" spoils",
+                StartTime = "08:00",
+                EndTime = "12:30",
+                TimeEntryTypeId = timeEntryTypes.First(x => x.Identifier == "Work").TimeEntryTypeId,
+                EntryDate = DateTime.Now,
+                UserEmploymentId = companyUser.UserEmployments.First().Id,
+                UnitTypeId = unitTypes.First(x => x.Key == "Hours").Id,
+                StatusId = statuses.First(x => x.Key == "Open").Id,
+            };
+
+            "Create TimeEntryRecord".Log();
+            var timeEntryRecordResponse = await gratisalClient.TimeEntry_CreateTimeEntryRecordAsync(timeEntryRecordRequest);
+
+            "Done creating TimeEntryRecord\n".Log();
+
+            #endregion
+
+            #region * Delete companyusers named Jack *
+            "* Delete companyusers named Jack *".Log();
+
+            "Get all company users".Log();
+            var companyUsers = await gratisalClient.CompanyUsers_GetAllCompanyUsersAsync();
+
+            "Find all users called Jack".Log();
+            var selectedCompanyUsers = companyUsers.Where(x => x.FirstName == "Jack");
+
+            $"Delete {selectedCompanyUsers.Count()} companyusers. Only companyusers without payslips can be deleted".Log();
+            foreach (var selectedCompanyUser in selectedCompanyUsers)
+            {
+                $"Deleting ({selectedCompanyUser.UserIdentityNumber}) {selectedCompanyUser.FirstName} {selectedCompanyUser.MiddleName} {selectedCompanyUser.LastName} from CompanyUsers".Log();
+                await gratisalClient.CompanyUsers_DeleteCompanyUserAsync(selectedCompanyUser.Id ?? 0);
+            }
+
+            "Done deleting companyusers named Jack\n".Log();
+
+            #endregion
+
+            "Terminate the session".Log();
             await gratisalClient.Close();
         }
     }
